33c6628c767037b4c22f0686256acac8a550aed3
Tom Grossi <TheAutomaTom@gmail.com>
2024-05-04 11:13:14 -0500

    Add Sass support

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+  <body data-theme="dark">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </body>
 </template>
 
-<style scoped>
+<style lang="scss">
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./App/Infra/Styles/index.css";
+import "./App/Infra/Styles/index.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
