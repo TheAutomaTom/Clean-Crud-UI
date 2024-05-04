fd400742d1e2e8ec4fb89109d085274f4115346e
Tom Grossi <TheAutomaTom@gmail.com>
2024-05-04 14:19:38 -0500

    Stub out layout components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ContentView from "./App/Views/ContentView.vue";
 import { onMounted, onUnmounted } from "vue";
 import { useAppState } from "./App/State/AppState";
 const app = useAppState();
@@ -17,15 +18,38 @@ function handleScroll() {
 </script>
 
 <template>
-  <body data-theme="dark" class="h-96">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
-    <span>{{ app.IsScrolled }}</span>
-  </body>
+  <content-view id="content-view" ></content-view>
 </template>
 
 <style lang="scss">
 
+#content-view {
+  @apply w-full;
+  @apply bg-slate-300;
+}
+
+// .app-wrapper {
+//   @apply w-full;
+// }
+
+// .buffer-header {
+//   @apply w-full;
+// }
+
+// $footer-height: 15em;
+// .content-wrapper {
+//   @apply w-full flex justify-center;
+//   padding-bottom: $footer-height;
+// }
+
+// .content {
+//   @apply w-full max-w-2xl flex;
+// }
+
+// .footer-wrapper {
+//   position: absolute;
+//   bottom: 0;
+//   width: 100%;
+//   height: $footer-height;
+// }
 </style>
---
+++ src/App/Views/BodyView/HeaderComp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import { useAppState } from "./stores/AppState";
+// import { onMounted, onUnmounted } from "vue";
+// import Header from "./infra/components/body/Header.vue";
+// const app$ = useAppState();
+</script>
+
+<template>  <!---- Template ------------------------------------------------>
+  <div>
+    
+    <span>"Howdy y'all"</span>
+
+  </div>
+</template>
+
+<style scoped lang="scss">  //---- Style ----------------------------------->
+</style>
