439f7dfdc63968f5246ef771c3e6c35499cd0bc7
Tom Grossi <TheAutomaTom@gmail.com>
2024-05-04 11:20:29 -0500

    Stub out AppState

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+import { useAppState } from "./App/State/AppState";
+const app = useAppState();
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+
+function handleScroll() {
+  app.IsScrolled = window.scrollY > 0;
+}
 
 </script>
 
 <template>
-  <body data-theme="dark">
+  <body data-theme="dark" class="h-96">
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    
+    <span>{{ app.IsScrolled }}</span>
   </body>
 </template>
 
